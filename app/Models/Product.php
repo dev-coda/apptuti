c35015f4eb0d5c4211b4ea4562efa59cfaed588e
David Correa <david.correav@gmail.com>
2024-08-20 22:19:39 -0500

    Changes to tax calculation

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -28,85 +28,102 @@ class Product extends  Model
         'variation_id',
         'is_combined',
         'parent_id',
-        
+
     ];
 
 
-    public function brand(){
+    public function brand()
+    {
         return $this->belongsTo(Brand::class);
     }
 
-    public function categories(){
+    public function categories()
+    {
         return $this->belongsToMany(Category::class);
     }
 
-    public function labels(){
+    public function labels()
+    {
         return $this->belongsToMany(Label::class)->whereActive(1);
     }
 
-    public function related(){
+    public function related()
+    {
         return $this->belongsToMany(Product::class, 'product_related', 'product_id', 'product_related_id')->orderBy('name');
     }
 
-    
-    public function tax(){
+
+    public function tax()
+    {
         return $this->belongsTo(Tax::class);
     }
 
-    public function variation(){
+    public function taxValue()
+    {
+        return $this->tax->tax;
+    }
+
+    public function variation()
+    {
         return $this->belongsTo(Variation::class);
     }
 
 
-    public function combinations(){
+    public function combinations()
+    {
         return $this->belongsToMany(Product::class, 'product_combination', 'parent_id', 'product_id')
             ->orderBy('name')
             ->withPivot(["price", "variation_item_id"]);
     }
 
 
-    public function items(){
+    public function items()
+    {
         return $this->belongsToMany(VariationItem::class, 'product_item_variation', 'product_id', 'variation_item_id')
             ->withPivot(["price", "enabled", 'sku']);
     }
 
 
-    public function images(){
+    public function images()
+    {
         return $this->hasMany(ProductImage::class);
     }
 
-    public function bonifications(){
+    public function bonifications()
+    {
         return $this->belongsToMany(Bonification::class);
     }
 
-    public function getImageAttribute(){
+    public function getImageAttribute()
+    {
         return $this->images->first()?->path;
     }
 
-    public function getFinalPriceAttribute(){
-       
+    public function getFinalPriceAttribute()
+    {
+
 
         $discount = $this->discount;
         $discount_on  = 'Producto';
 
 
-        if($this->brand){
-            if($this->brand->discount > $discount){
+        if ($this->brand) {
+            if ($this->brand->discount > $discount) {
                 $discount = $this->brand->discount;
                 $discount_on  = 'Marca';
             }
         }
- 
-        if($this->brand->vendors){
-            if($this->brand->vendors->discount > $discount){
+
+        if ($this->brand->vendors) {
+            if ($this->brand->vendors->discount > $discount) {
                 $discount = $this->brand->vendors->discount;
                 $discount_on  = 'Vendor';
             }
-        }   
+        }
 
-        
 
-        if($this->bonifications->count()){
+
+        if ($this->bonifications->count()) {
             $discount_on  = false;
             $discount = 0;
         }
@@ -114,38 +131,44 @@ class Product extends  Model
         $price = $this->price;
 
         $variation = $this->items?->first();
-        if($this->items?->first()){
-           $price = $variation->pivot->price;
+        if ($this->items?->first()) {
+            $price = $variation->pivot->price;
         }
 
         $has_discount = false;
-        if($discount > 0){
+        if ($discount > 0) {
             $has_discount = true;
         }
 
 
+        $pricePreTax = $price - ($price * $discount / 100);
+        $finalPrice = $this->taxValue() > 0 ? ($pricePreTax + ($pricePreTax * $this->taxValue() / 100)) : $pricePreTax;
+        $pricePreDiscount = $price + ($price * $this->taxValue() / 100);
+
+
         return [
-            'old'=>$price,
-            'price'=>($price - ($price * $discount / 100)),
-            'totalDiscount'=>($price * $discount / 100),
-            'discount'=>$discount,
-            'discount_on'=>$discount_on,
-            'has_discount'=>$has_discount
+            'old' => $pricePreDiscount,
+            'price' =>  $finalPrice,
+            'totalDiscount' => ($price * $discount / 100),
+            'discount' => $discount,
+            'discount_on' => $discount_on,
+            'has_discount' => $has_discount
         ];
     }
 
-    public function scopeActive($query){
+    public function scopeActive($query)
+    {
         return $query->where('active', 1);
     }
 
-    public function orders(){
+    public function orders()
+    {
         return $this->belongsToMany(Order::class, 'order_products');
     }
 
 
-    public function getCategoryAttribute(){
+    public function getCategoryAttribute()
+    {
         return $this->categories->first();
     }
-
-
 }
